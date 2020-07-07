270c6861330cc22bc2e13e2210de793e8c3530ab
Patrick Wilson <pmwilson333@gmail.com>
2020-07-07 15:42:11 -0700

    saved after changes

--- cookieUtils.js
+++ cookieUtils.js
@@ -2,3 +2,8 @@ export function isGuessCorrect(correctNumber, guessNumber) {
   if (correctNumber === guessNumber) return true;
   return false;
 }
+
+export function isGuessLow(correctNumber, guessNumber) {
+  if (correctNumber > guessNumber) return true;
+  return false;
+}
